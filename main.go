99d906e8de70b6b193871fb19aac5ac1a489fa94
Leo El-Solh <chudmaxxer445@gmail.com>
2026-09-10 16:21:34 +0100

    Hash scanner files with sha256

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 )
 
@@ -11,10 +13,32 @@ func main() {
 		if err != nil {
 			return err
 		}
-		fmt.Println(path)
+
+		if d.IsDir() {
+			if d.Name() == ".git" {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		hash, err := hashFile(path)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%s  %s\n", hash, path)
 		return nil
 	})
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func hashFile(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	sum := sha256.Sum256(data)
+	return fmt.Sprintf("%x", sum), nil
 }
